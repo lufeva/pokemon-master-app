652aff7a5b58eafb6f36f31c0e60f693256fe3a0
luisa-valencia-sp <luisafer144@gmail.com>
2024-10-02 20:48:08 -0500

    add ngrx root store

--- apps/pokemon-master-ui/src/app/app.config.ts
+++ apps/pokemon-master-ui/src/app/app.config.ts
@@ -4,9 +4,13 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(),
+    provideStore(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
